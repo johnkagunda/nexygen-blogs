939c1d8ac8b5dfa5b02cc4f91b67b64a875c8cb1
johnkagunda <118008558+johnkagunda@users.noreply.github.com>
2026-07-16 15:06:28 +0300

    Refactor Google sign-in logic and update UI

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -5,36 +5,42 @@ import { FcGoogle } from "react-icons/fc";
 import { useNavigate } from "react-router-dom";
 import { db } from "../config/firebase";
 
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+
 const OAuth = () => {
   const navigate = useNavigate();
+
   const signInWithGoogle = async () => {
     try {
-      const auth = getAuth();
-      const provider = new GoogleAuthProvider();
-      const userCredentials = await signInWithPopup(auth, provider);
-      const user = userCredentials.user;
-      await setDoc(doc(db, "users", user.uid), {
-        name: user.displayName,
-        email: user.email,
-        timestamp: serverTimestamp(),
-      });
-      if (user) {
-        toast.success("Successfully signed in!!");
-        console.log(user);
-        navigate("/");
-      }
+      const { user } = await signInWithPopup(auth, provider);
+
+      await setDoc(
+        doc(db, "users", user.uid),
+        {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        },
+        { merge: true }
+      );
+
+      toast.success("Successfully signed in!");
+      navigate("/");
     } catch (error) {
-      console.log(error);
+      toast.error("Google sign-in failed");
+      console.error(error.message);
     }
   };
+
   return (
-    <div className='mx-auto  my-8 w-full max-w-[70%] '>
+    <div className="mx-auto my-8 w-full max-w-[70%]">
       <button
         onClick={signInWithGoogle}
-        className='google__btn__shadow flex w-full cursor-pointer items-center justify-center rounded-md bg-gradient-to-r from-rose-400 to-red-500 py-3 font-semibold text-white transition duration-200 ease-in-out active:scale-95'
+        className="google__btn__shadow flex w-full items-center justify-center rounded-md bg-gradient-to-r from-rose-400 to-red-500 py-3 font-semibold text-white transition active:scale-95"
       >
-        <FcGoogle size={22} className='mr-2 rounded-full bg-white' />
-        Sign in with google
+        <FcGoogle size={22} className="mr-2 rounded-full bg-white" />
+        Sign in with Google
       </button>
     </div>
   );
